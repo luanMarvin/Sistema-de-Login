a32e95719880c0faffb1f877107ef0500df6ad2b
luanMarvin <luanmarvin@gmail.com>
2023-02-28 18:40:20 -0300

    Adiçao do regex para formulario e mudanças no fetch para habilitaçao do CORS

--- client/src/script/main.js
+++ client/src/script/main.js
@@ -38,10 +38,55 @@ function registerNewUser(){
     })
 }
 
-function verifyAndSend(){
-    if(passwordInput.value.length > 5 && firstNameInput.value.length > 0 && 
-        surnameInput.value.length > 0 && emailInput.value.length > 3){
-        console.log("approved")
+function verifyEmail(email){
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+}
+
+function verifyPassword(password) {
+    const regex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    return regex.test(password);
+  }
+
+  function verifyAndSend(){
+    let verifiedEmail = verifyEmail(emailInput.value);
+    let verifiedPassword = verifyPassword(passwordInput.value);
+
+    if(verifiedPassword == true && firstNameInput.value.length > 0 && 
+        surnameInput.value.length > 0 && verifiedEmail == true){
+            firstName = firstNameInput.value;
+            surName = surnameInput.value;
+            email = emailInput.value;
+            password = passwordInput.value;
+            data = {firstName, surName, email, password};
+            fetch("http://127.0.0.1:8080/api/users",
+        {
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              'Access-Control-Allow-Origin': 'http://127.0.0.1:5500/'
+            },
+            method: "POST",
+            body: JSON.stringify(data)
+        })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Não foi Possivel se conectar');
+          }
+          return response.json();
+        })
+        .then(data => {
+          if (data.success) {
+            alertShow.innerHTML = "Usuário registrado com sucesso!";
+          } else if (data.error) {
+            alertShow.innerHTML = "Ocorreu um erro ao registrar o usuário: " + data.error;
+          } else {
+            alertShow.innerHTML = "Ocorreu um erro ao registrar o usuário.";
+          }
+        })
+        .catch(error => {
+          console.error('There was a problem with the fetch operation:', error);
+        });
     } else {
         alertShow.innerHTML = "É necessário registrar os dados acima de maneira correta"
     }
