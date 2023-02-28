910d688098358cb82e1aeb39f03a71aef52a6c6a
luanMarvin <luanmarvin@gmail.com>
2023-02-28 18:36:27 -0300

    Adiçao do regex para formulario e mudanças no fetch para habilitaçao do CORS

--- server/assets/routes/index.js
+++ server/assets/routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const UserController = require('../controllers');
+const cors = require('cors')
 
-router.post('/users', UserController.post)
+router.post('/users', cors(), UserController.post)
 
 module.exports = router
--- server/server.js
+++ server/server.js
@@ -6,10 +6,11 @@ const router = require('./assets/routes');
 const app = express();
 app.use(express.json());
 app.use('/api', router);
+
 var corsConfig = {
-    origin: ["http://127.0.0.1:5500"],
+    origin: ["http://127.0.0.1:5500/"],
     optionsSuccessStatus: 200
-}
+};
 app.use(cors(corsConfig));
 
 const URI = "mongodb://localhost:27017/sistema-de-login";
