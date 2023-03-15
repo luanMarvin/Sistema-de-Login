01f43e9e5c13264e2c057fea9cad1e5eda2f366d
luanMarvin <luanmarvin@gmail.com>
2023-03-15 20:05:19 -0300

    Pequena refatoraçao

--- server/assets/routes/index.js
+++ server/assets/routes/index.js
@@ -4,9 +4,4 @@ const UserController = require('../controllers');
 router.post('/users', UserController.post);
 router.get('/users/:email', UserController.getTestEmail);
 
-//Errors
-router.get('*', function(req, res){
-    res.status(404).render(path.join(__dirname + '/page-not-found.html'));
-  });
-
 module.exports = router
--- server/server.js
+++ server/server.js
@@ -11,18 +11,14 @@ const corsConfig = {
     origin: ['http://localhost:5500','http://127.0.0.1:5500'],
     optionsSuccessStatus: 200,
 };
-app.use(cors(corsConfig)); //CORS - Disabled
+app.use(cors(corsConfig)); //CORS - Enabled
 app.use(express.json()); //Parsing Middleware
 app.use('/api', router); //API routes
 
-//Database Connection
-const URI = "mongodb://localhost:27017/sistema-de-login";
+const URI = "mongodb://localhost:27017/sistema-de-login"; //Database URI
 connect(URI);
 
 
 //Port
 const port = process.env.PORT || 8080;
 app.listen(port, () =>{console.log(`Server is running at port ${port}`)});
-
-//Exports
-module.exports = URI
