55cd3e828527fae02b3e5ca93599e24cb4e71adb
luanMarvin <luanmarvin@gmail.com>
2023-03-27 23:18:54 -0300

    Recebendo informaçao do back-end

---
+++ client/src/script/profile.js
@@ -0,0 +1,17 @@
+const token = localStorage.getItem('token');
+
+async function getData(token) {
+    fetch('http://127.0.0.1:8080/api/autenticated', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+  }
+
+getData(token);
